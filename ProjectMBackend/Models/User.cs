ed469f9bdee8481491f9cbab1326ede685180c2c
Kaikeeksr <kaikeksr@outlook.com>
2024-12-22 18:47:10 -0300

    Configurando CORS + adequando classe para receber req do front

--- ProjectMBackend/Models/User.cs
+++ ProjectMBackend/Models/User.cs
@@ -15,7 +15,6 @@ namespace ProjectMBackend.Models
         public required string Username { get; set; }
         public required string Password { get; set; }
         public required string Email { get; set; }
-        public required string DateOfBirth { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }

388e8fc38a5efed93e380b4daee5fb09b00c7762
Kaikeeksr <kaikeksr@oulook.com>
2025-01-21 12:14:22 -0300

    Encapsulando lógica do login

--- ProjectMBackend/Endpoints/User/UserLogin.cs
+++ ProjectMBackend/Endpoints/User/UserLogin.cs
@@ -7,35 +7,12 @@ namespace ProjectMBackend.Endpoints.User
     {
         public static void Map(WebApplication app)
         {
-            app.MapPost("/User/Login", async (Models.Login l, IMongoDatabase db, Auth auth) =>
+            app.MapPost("/User/Login", async (Login l, IMongoDatabase db, Auth auth) =>
             {
-                var userCollection = db.GetCollection<Models.User>("users");
-                var user = await userCollection.Find(x => x.Username == l.Username).FirstOrDefaultAsync();
-
-                if (user == null || !VerifyPass(l.Password, user))
-                {
-                    return Results.BadRequest(
-                    new
-                    {
-                        msg = $"Username ou senha inválidos",
-                        status = "NOT_OK"
-                    });
-                }
-
-                var token = auth.GenerateJwt(user);
-
-                return Results.Ok(new
-                {
-                    message = "Login efetuado com sucesso",
-                    token = token
-                });
-
-            });
-        }
-
-        private static bool VerifyPass(string pass, Models.User user)
-        {
-            return BCrypt.Net.BCrypt.Verify(pass, user.Password);
+                return await l.SignIn(db, auth);
+            })
+            .Produces<Login.LoginResponse>(StatusCodes.Status200OK)
+            .Produces<Login.LoginResponse>(StatusCodes.Status400BadRequest);
         }
     }
 }
--- ProjectMBackend/Models/Login.cs
+++ ProjectMBackend/Models/Login.cs
@@ -1,8 +1,47 @@
-﻿namespace ProjectMBackend.Models
+﻿using MongoDB.Driver;
+using ProjectMBackend.AuthModel;
+using ProjectMBackend.Models;
+
+public class Login
 {
-    public class Login
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+
+    public class LoginResponse
     {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
+        public string Message { get; set; }
+        public string Status { get; set; }
+        public string? Token { get; set; }
+    }
+
+    public async Task<IResult> SignIn(IMongoDatabase db, Auth auth)
+    {
+        var userCollection = db.GetCollection<User>("users");
+        var user = await userCollection.Find(x => x.Username == Username)
+                                     .FirstOrDefaultAsync();
+
+        if (user == null || !VerifyPassword(Password, user))
+        {
+            return Results.BadRequest(new LoginResponse
+            {
+                Message = "Username ou senha inválidos",
+                Status = "NOT_OK",
+                Token = null
+            });
+        }
+
+        var token = auth.GenerateJwt(user);
+
+        return Results.Ok(new LoginResponse
+        {
+            Message = "Login efetuado com sucesso",
+            Status = "OK",
+            Token = token
+        });
+    }
+
+    private static bool VerifyPassword(string password, User user)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, user.Password);
     }
 }
